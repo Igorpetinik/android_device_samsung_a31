aabecb24d90de82ba3c0dc1d17c2f38c6c580103
IgorechekXD <148623401+Igorpetinik@users.noreply.github.com>
2024-08-25 14:17:01 +0300

    Add files via upload

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -6,28 +6,13 @@
 
 LOCAL_PATH := $(call my-dir)
 
-# Rootdir of System and Recovery
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.recovery.mt6768.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.recovery.mt6768.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
+# Rootdir of System
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := ueventd.mt6768.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/ueventd.mt6768.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.recovery.samsung.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.recovery.samsung.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
